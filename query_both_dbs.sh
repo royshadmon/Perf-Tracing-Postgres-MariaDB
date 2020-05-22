2ef29aca806bdb74617a22c3d5e453f26dca7dbc
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-22 14:43:30 -0700

    query typo fix

--- query_both_dbs.sh
+++ query_both_dbs.sh
@@ -1,6 +1,6 @@
 # Primary key lookup
-psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students WHERE emp_id = 10;' > psql-primary-key-lookup.txt
-mysql -u roy -D project -e 'SELECT * FROM students WHERE emp_id = 10;' > mariadb-primary-key-lookup.txt
+psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students WHERE student_id = 10;' > psql-primary-key-lookup.txt
+mysql -u roy -D project -e 'SELECT * FROM students WHERE student_id = 10;' > mariadb-primary-key-lookup.txt
 
 # Secondary index lookup
 psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students WHERE dept_no = 10;' > psql-secondary-index-lookup.txt
@@ -9,3 +9,7 @@ mysql -u roy -D project -e 'SELECT * FROM students WHERE dept_no = 10;' > mariad
 # full scan
 psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students;' > psql-full-scan.txt
 mysql -u roy -D project -e 'SELECT * FROM students;' > mariadb-full-scan.txt
+
+
+#student_id int,
+    # dept_no char(50),
