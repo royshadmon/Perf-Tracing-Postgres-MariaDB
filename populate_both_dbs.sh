f5de9dbd0a4b35e52f6091ad3d66f98db987eb1f
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-22 14:50:27 -0700

    checking for max rows argument

--- populate_both_dbs.sh
+++ populate_both_dbs.sh
@@ -5,6 +5,7 @@ max_rows=$1
 if [ $# -ne 1 ]
   then
     echo "Input 1 argument which represents how many rows to populate the dbs"
+    exit
 fi
 
 export PGPASSWORD='demo'; psql -U 'postgres' -d 'postgres' -c 'DROP TABLE IF EXISTS students;'
