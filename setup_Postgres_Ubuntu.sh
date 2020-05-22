e0a68f6797a53ebd8ff664d59473f80118815d41
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-22 14:45:37 -0700

    fixing setup script

--- setup_Postgres_Ubuntu.sh
+++ setup_Postgres_Ubuntu.sh
@@ -7,7 +7,7 @@ sudo apt install postgresql postgresql-contrib
 psql -U 'postgres' -d 'postgres' -c 'ÇREATE USER roy;'
 # Grant user privileges (gives full access to user roy)
 psql -U 'postgres' -d 'postgres' -c 'GRANT ALL ON ALL TABLES IN SCHEMA public TO roy;'
-psql -U 'postgres' -d 'postgres' -c 'GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO mygrp;'
+psql -U 'postgres' -d 'postgres' -c 'GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO roy;'
 
 
 
