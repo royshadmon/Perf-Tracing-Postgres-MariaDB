0503b702e3c88830c00bc2a79e8a34f3ab23ac24
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-26 11:11:05 -0700

    automating perf tests

--- run_primaryKey_index_tests.sh
+++ run_primaryKey_index_tests.sh
@@ -4,7 +4,9 @@ num_rows=1
 
 while [ $num_rows -le 10000 ]
 do
-	bash populate_both_dbs $num_rows
-	bash query_both_dbs.sh
+	# bash populate_both_dbs $num_rows
+	# bash query_both_dbs.sh
+	mkdir $num_rows
+	mv *.txt $num_rows
 	num_rows=$((num_rows * 10))
 done
