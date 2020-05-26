d6e05e3b5f69798f5c4e013880877498cd0d7c7a
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-26 10:53:14 -0700

    add perf stat in query

--- query_both_dbs.sh
+++ query_both_dbs.sh
@@ -1,14 +1,14 @@
 # Primary key lookup
-psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students WHERE student_id = 10;' > psql-primary-key-lookup.txt
-mysql -u roy -D project -e 'SELECT * FROM students WHERE student_id = 10;' > mariadb-primary-key-lookup.txt
+perf stat psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students WHERE student_id = 10;' > psql-primary-key-lookup.txt
+perf stat mysql -u roy -D project -e 'SELECT * FROM students WHERE student_id = 10;' > mariadb-primary-key-lookup.txt
 
 # Secondary index lookup
-psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students WHERE dept_no = 1;' > psql-secondary-index-lookup.txt
-mysql -u roy -D project -e 'SELECT * FROM students WHERE dept_no = 1;' > mariadb-secondary-index-lookup.txt
+perf stat psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students WHERE dept_no = 1;' > psql-secondary-index-lookup.txt
+perf stat mysql -u roy -D project -e 'SELECT * FROM students WHERE dept_no = 1;' > mariadb-secondary-index-lookup.txt
 
 # full scan
-psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students;' > psql-full-scan.txt
-mysql -u roy -D project -e 'SELECT * FROM students;' > mariadb-full-scan.txt
+perf stat psql -U 'postgres' -d 'postgres' -c 'SELECT * FROM students;' > psql-full-scan.txt
+perf stat mysql -u roy -D project -e 'SELECT * FROM students;' > mariadb-full-scan.txt
 
 
 #student_id int,
