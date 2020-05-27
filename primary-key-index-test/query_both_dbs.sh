08c4559a134a3505e9a3c1a86c0796ab83a9a39a
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-27 14:57:52 -0700

    consistent query method

--- primary-key-index-test/query_both_dbs.sh
+++ primary-key-index-test/query_both_dbs.sh
@@ -1,12 +1,12 @@
 # Primary key lookup
-perf stat -o psql-primary-key-lookup.txt psql -U 'root' -d 'postgres' -c 'SELECT * FROM students WHERE student_id = 10;'  
-perf stat -o mariadb-primary-key-lookup.txt mysql -u root -D project -e 'SELECT * FROM students WHERE student_id = 10;' 
+perf stat -o psql-primary-key-lookup.txt sudo nice -n -5 taskset -c 1 psql -U 'root' -d 'postgres' -c 'SELECT * FROM students WHERE student_id = 10;'  
+perf stat -o mariadb-primary-key-lookup.txt sudo nice -n -5 taskset -c 1 mysql -u root -D project -e 'SELECT * FROM students WHERE student_id = 10;' 
 
 # Secondary index lookup
-perf stat -o psql-secondary-index-lookup.txt psql -U 'root' -d 'postgres' -c 'SELECT * FROM students WHERE dept_no = 1;' 
-perf stat -o mariadb-secondary-index-lookup.txt mysql -u root -D project -e 'SELECT * FROM students WHERE dept_no = 1;' 
+perf stat -o psql-secondary-index-lookup.txt sudo nice -n -5 taskset -c 1 psql -U 'root' -d 'postgres' -c 'SELECT * FROM students WHERE dept_no = 1;' 
+perf stat -o mariadb-secondary-index-lookup.txt sudo nice -n -5 taskset -c 1 mysql -u root -D project -e 'SELECT * FROM students WHERE dept_no = 1;' 
 
 # full scan
-perf stat -o psql-full-scan.txt psql -U 'root' -d 'postgres' -c 'SELECT * FROM students;'  
-perf stat -o mariadb-full-scan.txt mysql -u root -D project -e 'SELECT * FROM students;' 
+perf stat -o psql-full-scan.txt sudo nice -n -5 taskset -c 1 psql -U 'root' -d 'postgres' -c 'SELECT * FROM students;'  
+perf stat -o mariadb-full-scan.txt sudo nice -n -5 taskset -c 1 mysql -u root -D project -e 'SELECT * FROM students;' 
 
