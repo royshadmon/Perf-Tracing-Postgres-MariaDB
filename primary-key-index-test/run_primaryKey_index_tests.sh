cb150d5837d48a4c8c99b48b7a5639863512ece8
royshadmon <rshadmon@gmail.com>
2020-05-28 14:23:03 -0700

    removing completed tests

--- primary-key-index-test/run_primaryKey_index_tests.sh
+++ primary-key-index-test/run_primaryKey_index_tests.sh
@@ -4,7 +4,7 @@ num_rows=1
 
 while [ $num_rows -le 10000 ]
 do
-	bash populate_both_dbs $num_rows
+	bash populate_both_dbs.sh $num_rows
 	bash query_both_dbs.sh
 	mkdir $num_rows
 	mv *.txt $num_rows
