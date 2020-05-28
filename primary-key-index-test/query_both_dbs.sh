1bbab6a1522ad534600217f05bd3b46358c4eb6f
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-28 09:40:02 -0700

    another test

--- primary-key-index-test/query_both_dbs.sh
+++ primary-key-index-test/query_both_dbs.sh
@@ -1,12 +1,4 @@
-# Primary key lookup
-perf stat -o psql-primary-key-lookup.txt sudo nice -n -5 taskset -c 1 psql -U 'root' -d 'postgres' -c 'SELECT * FROM students WHERE student_id = 10;'  
-perf stat -o mariadb-primary-key-lookup.txt sudo nice -n -5 taskset -c 1 mysql -u root -D project -e 'SELECT * FROM students WHERE student_id = 10;' 
-
-# Secondary index lookup
-perf stat -o psql-secondary-index-lookup.txt sudo nice -n -5 taskset -c 1 psql -U 'root' -d 'postgres' -c 'SELECT * FROM students WHERE dept_no = 1;' 
-perf stat -o mariadb-secondary-index-lookup.txt sudo nice -n -5 taskset -c 1 mysql -u root -D project -e 'SELECT * FROM students WHERE dept_no = 1;' 
-
-# full scan
-perf stat -o psql-full-scan.txt sudo nice -n -5 taskset -c 1 psql -U 'root' -d 'postgres' -c 'SELECT * FROM students;'  
-perf stat -o mariadb-full-scan.txt sudo nice -n -5 taskset -c 1 mysql -u root -D project -e 'SELECT * FROM students;' 
+# ROLLBACK 
+perf stat -o psql-rollback.txt -r 10 -e branches -e branch-misses -e cycles -e instructions -e ref-cycles -e cpu-clock -e faults -e task-clock -e L1-dcache-load-misses -e L1-icache-load-misses -e branch-load-misses -e l2_rqsts.code_rd_miss sudo nice -n -5 taskset -c 1 psql -U 'root' -d 'postgres' -f psql_transaction.sql
+perf stat -o mariadb-rollback.txt sudo nice -n -5 taskset -c 1 mysql -u root -D project < mariaDB_transaction.sql
 
---
+++ transaction-test/populate_both_dbs.sh
@@ -0,0 +1,49 @@
+# populate Postgres
+
+max_rows=$1
+
+if [ $# -ne 1 ]
+  then
+    echo "Input 1 argument which represents how many rows to populate the dbs"
+    exit
+fi
+
+psql -U 'postgres' -d 'postgres' -c 'DROP TABLE IF EXISTS students;'
+psql -U 'postgres' -d 'postgres' -c "CREATE TABLE students (
+    student_id int,
+    dept_no int,
+    CONSTRAINT student_pk PRIMARY KEY (student_id)
+); CREATE INDEX student_dept_idx ON students (dept_no);"
+
+for (( student_id=1; student_id<=$max_rows; student_id++ ))
+do
+ if [ $((student_id % 2)) -eq 0 ]
+ then
+ 	dept="1"  
+ else
+  	dept="2"  
+fi 
+psql -U 'postgres' -d 'postgres' -c "INSERT INTO students VALUES($student_id,$dept);"
+done
+
+# populate MariaDB
+mysql -u roy -D project -e 'DROP TABLE IF EXISTS students;'
+mysql -u roy -D project -e "CREATE TABLE students (
+    student_id int,
+    dept_no int,
+    CONSTRAINT student_pk PRIMARY KEY (student_id)
+); CREATE INDEX student_dept_idx ON students (dept_no);"
+
+for (( student_id=1; student_id<=$max_rows; student_id++ ))
+do
+ if [ $((student_id % 2)) -eq 0 ]
+ then
+ 	dept="1"  
+ else
+  	dept="2"  
+fi 
+mysql -u roy -D project -e "INSERT INTO students VALUES($student_id,$dept);"
+done
+
+
+
