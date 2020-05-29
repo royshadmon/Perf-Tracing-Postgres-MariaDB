ed002c8b3c68eea21cda26fd26add062c5058702
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-29 13:11:18 -0700

    partition test

---
+++ key-partition-test/populate_both_dbs.sh
@@ -0,0 +1,43 @@
+
+
+
+# populate Postgres
+
+max_rows=$1
+
+if [ $# -ne 1 ]
+  then
+    echo "Input 1 argument which represents how many rows to populate the dbs"
+    exit
+fi
+
+psql -U 'postgres' -d 'postgres' -c 'DROP TABLE IF EXISTS members;'
+psql -U 'postgres' -d 'postgres' -c "CREATE TABLE members (
+    member_id int,
+    joined DATE NOT NULL
+);"
+
+d="2014-06-29"
+for (( member_id=1; member_id<=$max_rows; member_id++ ))
+do
+	d=$(date -I -d "$d + 1 day")
+psql -U 'postgres' -d 'postgres' -c "INSERT INTO members VALUES($student_id,$d);"
+done
+
+# populate MariaDB
+mysql -u roy -D project -e 'DROP TABLE IF EXISTS members;'
+mysql -u roy -D project -e "CREATE TABLE members (
+    member_id int,
+    joined DATE NOT NULL
+)
+PARTITION BY KEY(joined)
+PARTITIONS 6;"
+d="2014-06-29"
+for (( member_id=1; member_id<=$max_rows; member_id++ ))
+do
+ d=$(date -I -d "$d + 1 day")
+mysql -u roy -D project -e "INSERT INTO members VALUES($member_id,$d);"
+done
+
+
+
--- primary-key-index-test/query_both_dbs.sh
+++ primary-key-index-test/query_both_dbs.sh
@@ -3,7 +3,7 @@
 
 # Primary key lookup
 perf stat -o psql-primary-key-lookup.txt --all-cpus -r 10 -e branches -e branch-misses -e cycles -e instructions -e ref-cycles -e cpu-clock -e faults -e task-clock -e L1-dcache-load-misses -e L1-icache-load-misses -e branch-load-misses -e l2_rqsts.code_rd_miss -e l2_rqsts.miss sudo nice -n -5 taskset -c 1 sudo psql -U 'roy' -d 'postgres' -c 'SELECT * FROM students WHERE student_id = 10;'  
-perf stat -o mariadb-primary-key-lookup.txt sudo nice -n -5 taskset -c 1 sudo mysql -u root -D project -e 'SELECT * FROM students WHERE student_id = 10;' 
+perf stat -o mariadb-primary-key-lookup.txt --all-cpus -r 10 -e branches -e branch-misses -e cycles -e instructions -e ref-cycles -e cpu-clock -e faults -e task-clock -e L1-dcache-load-misses -e L1-icache-load-misses -e branch-load-misses -e l2_rqsts.code_rd_miss -e l2_rqsts.miss sudo nice -n -5 taskset -c 1 sudo mysql -u root -D project -e 'SELECT * FROM students WHERE student_id = 10;' 
 
 # Secondary index lookup
 perf stat -o psql-secondary-index-lookup.txt --all-cpus -r 10 -e branches -e branch-misses -e cycles -e instructions -e ref-cycles -e cpu-clock -e faults -e task-clock -e L1-dcache-load-misses -e L1-icache-load-misses -e branch-load-misses -e l2_rqsts.code_rd_miss -e l2_rqsts.miss sudo nice -n -5 taskset -c 1 sudo psql -U 'roy' -d 'postgres' -c 'SELECT * FROM students WHERE dept_no = 1;' 
