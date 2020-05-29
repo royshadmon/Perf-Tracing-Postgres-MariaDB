a010a564d8a82c694a837a7cb7508b142d2da514
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-29 13:18:49 -0700

    fixing query

--- key-partition-test/run_partition_test.sh
+++ key-partition-test/run_partition_test.sh
@@ -5,7 +5,7 @@ num_rows=1
 while [ $num_rows -le 10000 ]
 do
 	bash populate_both_dbs.sh $num_rows
-	bash query_both_dbs.sh $((num_rows/2))
+	bash query_both_dbs.sh 
 	mkdir $num_rows
 	mv *.txt $num_rows
 	num_rows=$((num_rows * 10))
