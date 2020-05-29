b2fca36c9cc30b0584bc4a7b6b96da10651cf18f
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-05-29 14:00:29 -0700

    updating query in for partition

--- key-partition-test/populate_both_dbs.sh
+++ key-partition-test/populate_both_dbs.sh
@@ -14,29 +14,46 @@ fi
 psql -U 'postgres' -d 'postgres' -c 'DROP TABLE IF EXISTS members;'
 psql -U 'postgres' -d 'postgres' -c "CREATE TABLE members (
     member_id int,
+    gender char(1),
     joined DATE NOT NULL
 );"
 
 d="2014-06-29"
 for (( member_id=1; member_id<=$max_rows; member_id++ ))
 do
+	if [ $((member_id % 2)) -eq 0 ]
+	then
+		gender='M'
+	else
+		gender='F'
+	fi
 	d=$(date -I -d "$d + 1 day")
-psql -U 'postgres' -d 'postgres' -c "INSERT INTO members VALUES($member_id,'$d');"
+	psql -U 'postgres' -d 'postgres' -c "INSERT INTO members VALUES($member_id, $gender, '$d');"
 done
 
 # populate MariaDB
 mysql -u roy -D project -e 'DROP TABLE IF EXISTS members;'
 mysql -u roy -D project -e "CREATE TABLE members (
     member_id int,
+    gender char(1),
     joined DATE NOT NULL
 )
-PARTITION BY KEY(joined)
-PARTITIONS 6;"
+PARTITION BY LIST COLUMNS (sector) (
+	PARTITIONS m VALUES IN ('M'),
+	PARTITIONS f VALUES IN ('F');"
+)
+
 d="2014-06-29"
 for (( member_id=1; member_id<=$max_rows; member_id++ ))
 do
- d=$(date -I -d "$d + 1 day")
-mysql -u roy -D project -e "INSERT INTO members VALUES($member_id,'$d');"
+	if [ $((member_id % 2)) -eq 0 ]
+	then
+		gender='M'
+	else
+		gender='F'
+	fi
+ 	d=$(date -I -d "$d + 1 day")
+	mysql -u roy -D project -e "INSERT INTO members VALUES($member_id, $gender, '$d');"
 done
 
 
