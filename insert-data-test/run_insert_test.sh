11b9280c5aec61854c9e6ca64fa6b8ab39e2412b
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2020-06-01 20:22:08 -0700

    run test script

---
+++ insert-data-test/run_insert_test.sh
@@ -0,0 +1,10 @@
+
+num_rows=1
+while [ $num_rows -le 10000 ]
+do
+	perf stat -o psql-insert.txt -I 100 --all-cpus -e branches -e branch-misses -e cycles -e instructions -e ref-cycles -e cpu-clock -e faults -e task-clock -e L1-dcache-load-misses -e L1-icache-load-misses -e branch-load-misses -e l2_rqsts.code_rd_miss -e l2_rqsts.miss sudo nice -n -5 taskset -c 1 sudo bash populate_postgres.sh $num_rows
+	perf stat -o psql-insert.txt -I 100 --all-cpus -e branches -e branch-misses -e cycles -e instructions -e ref-cycles -e cpu-clock -e faults -e task-clock -e L1-dcache-load-misses -e L1-icache-load-misses -e branch-load-misses -e l2_rqsts.code_rd_miss -e l2_rqsts.miss sudo nice -n -5 taskset -c 1 sudo bash populate_mariaDB.sh $num_rows	
+	mkdir $num_rows
+	mv *.txt $num_rows
+	num_rows=$((num_rows * 10))
+done
